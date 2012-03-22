2408b2fc947efbbaa8c0ec7ca20611fb5cf4f773
Vimarsh Raina <me@vimarshraina.com>
2012-03-22 15:25:13 +0000

    Removed the redundancy

--- Test Project/ZPP Prototype V1/FrameRate.h
+++
@@ -1,5 +0,0 @@
-#include "main.h"
-
-#define GameFrameRate 1000
-
-bool AnimateNextFrame(int desiredFrameRate, long long int &lastTime);
--- Test Project/ZPP Prototype V1/bmp.h
+++
@@ -1,7 +0,0 @@
-#ifndef _BMP_H				// If we haven't included this file							
-#define _BMP_H				// Set a flag saying we included it
-
-void DeletePics(HBITMAP &hBitmap);
-HBITMAP LoadABitmap(LPSTR szFileName);
-
-#endif
